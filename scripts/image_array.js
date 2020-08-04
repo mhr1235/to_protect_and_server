d7d3605de70655381180c53c9c1161c409896e18
mhr1235 <65504141+mhr1235@users.noreply.github.com>
2020-08-04 11:20:43 -0400

    Add files via upload

---
+++ scripts/image_array.js
@@ -0,0 +1,141 @@
+/*
+  
+        _   _  ___       _____       _____  ___    ___      
+/'\_/`\( ) ( )|  _`\    (  _  )     (___  )(  _`\ (  _`\    
+|     || |_| || (_) )   `\  ,/'         | || (_) )| ( (_)   
+| (_) ||  _  || ,  /     /'_`\/\     _  | ||  _ <'| |  _    
+| | | || | | || |\ \    | (_> ,<`   ( )_| || (_) )| (_( )   
+(_) (_)(_) (_)(_) (_)   `\___/\/'   `\___/'(____/'(____/'   
+                                                            
+                                                            
+   __     __      __     __                                 
+ /'__`\ /' _`\  /'__`\ /' _`\                               
+(_)  ) )| ( ) |(_)  ) )| ( ) |                              
+   /' / | | | |   /' / | | | |                              
+ /' /( )| (_) | /' /( )| (_) |                              
+(_____/'`\___/'(_____/'`\___/'                              
+                                                            
+                                                            
+*/
+
+
+// don't use new Array(3), just use []
+var numImages = 21;
+
+var images = [];
+
+var click_counter = 0;
+
+var random_test;
+
+
+
+
+
+//var i, images = [];
+// use push, don't manually assign the index
+/*images.push("images/image_2.jpg");
+images.push("images/image_3.jpg");
+images.push("images/image_4.jpg");
+images.push("images/image_5.jpg");
+images.push("images/image_6.jpg");
+images.push("images/image_7.jpg");
+images.push("images/image_8.jpg");
+images.push("images/image_9.jpg");
+images.push("images/image_10.jpg");
+images.push("images/image_11.jpg");
+images.push("images/image_12.jpg");
+images.push("images/image_13.jpg");
+images.push("images/image_14.jpg");
+images.push("images/image_15.jpg");
+images.push("images/image_16.jpg");
+images.push("images/image_17.jpg");
+images.push("images/image_18.jpg");
+images.push("images/image_19.jpg");
+images.push("images/image_20.jpg");
+images.push("images/image_21.jpg");*/
+
+//just add more images to creeate a larger pool
+
+
+for(let a=1; a<=numImages;a++){
+    images.push("images/image_"+a+".jpg");
+    //console.log(a);
+
+}
+
+
+images.sort(function ()
+{
+    return 0.5 - Math.random();
+});
+
+
+
+for (var i = 0; i < 9; i++){
+
+document.getElementsByClassName('captcha-image')[i].innerHTML = "<img src='"+images[i]+"'>";
+document.getElementsByClassName('captcha-image')[i].addEventListener("click",clicks);
+
+
+}
+
+
+
+ 
+
+
+function clicks(){
+
+
+	if(!$(this).hasClass("active")){
+      //alert("button was clicked");
+      click_counter++;
+//console.log(click_counter);
+
+	}
+	if($(this).hasClass("active")){
+      //alert("button was clicked");
+      click_counter--;
+//console.log(click_counter);
+
+	}
+}
+
+   document.getElementById('verify').onclick = function() {
+
+random_test = Math.floor((Math.random() * 8)+ 1);
+console.log(random_test);
+
+if(click_counter == random_test){
+
+//window.location.href = 'https://www1.nyc.gov/site/nypd/about/about-nypd/about-nypd-landing.page';
+window.location.href = 'p2.html';
+
+
+}
+if(click_counter != random_test && click_counter > 1){
+	window.location.href = 'p3.html';
+}
+if(click_counter == 0){
+location.reload();
+}
+};
+
+
+//console.log(counted);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
